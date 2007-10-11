f5571ee49dd43a8441341b5def66f6dcd9c111f8
Andrew Sterling Hanenkamp <hanenkamp@cpan.org>
2007-10-11 06:04:37 +0000

    Import of HANENKAMP/Bot-Net-0.0.3 from CPAN.
    gitpan-cpan-distribution: Bot-Net
    gitpan-cpan-version:      0.0.3
    gitpan-cpan-path:         HANENKAMP/Bot-Net-0.0.3.tar.gz
    gitpan-cpan-author:       HANENKAMP
    gitpan-cpan-maturity:     released

--- lib/Bot/Net/Mixin/Bot/IRCD.pm
+++ lib/Bot/Net/Mixin/Bot/IRCD.pm
@@ -1,68 +1,86 @@
 use strict;
 use warnings;
 
-package Bot::Net::Mixin::Bot::IRC;
+package Bot::Net::Mixin::Bot::IRCD;
 
 use Bot::Net::Mixin;
 
-use POE qw/ Component::IRC::State /;
 use Scalar::Util qw/ reftype /;
 
 =head1 NAME
 
-Bot::Net::Mixin::Bot::IRC - mixin class for building IRC bots
+Bot::Net::Mixin::Bot::IRCD - mixin class for building IRC daemon bots
 
 =head1 SYNOPSIS
 
-  # Build an Eliza chatbot for IRC
+  # Build an Eliza chatbot directly on the server
   use strict;
   use warnings;
-  package MyBotNet::Bot::Eliza;
+
+  package MyBotNet::Bot::ElizaOnServer;
 
   use Bot::Net::Bot;
-  use Bot::Net::Mixin::Bot::IRC;
+  use Bot::Net::Mixin::Bot::IRCD;
   use Chatbot::Eliza; # available separately on CPAN
+
+  on _start => run {
+      # Make it easy for other sessions to talk to us
+      get(KERNEL)->alias_set('eliza');
+  };
 
   on bot startup => run {
       remember eliza => Chatbot::Eliza->new;
   };
 
   on bot message_to_me => run {
-      my $event = get ARG0;
+      my $event = getARG0;
 
       my $reply = recall('eliza')->transform( $event->message );
       yield reply_to_sender => $message, $reply;
+  };
+
+  on bot quit => run {
+      # Clear the session alias for clean shutdown
+      get(KERNEL)->alias_remove('eliza');
+  };
+
+  1;
+
+Then in your server, you need something like this:
+
+  use strict;
+  use warnings;
+
+  package MyBotNet::Server::Main;
+
+  use Bot::Net::Server;
+  use Bot::Net::Mixin::Server::IRC;
+
+  use MyBotNet::Bot::ElizaOnServer;
+
+  on _start => run {
+      remember eliza => MyBotNet::Bot::ElizaOnServer->setup;
+  };
+
+  on server quit => run {
+
+      # Clean up for clean shutdown
+      forget 'eliza';
   };
 
   1;
 
 =head1 DESCRIPTION
 
-This is the mixin-class for L<Bot::Net> IRC bots. You "inherit" all the features of this mixin by using the class:
+Some bots are best run direclty on the server itself. This is generally useful for handling channel or nick management services and other administrative tasks. A typical bot should run as a separate entity whenever possible. See L<Bot::Net::Mixin::Bot::IRC>.
 
-  use Bot::Net::Bot;             # define a Bot::Net bot
-  use Bot::Net::Mixin::Bot::IRC; # become an IRC bot
+Unlike stand-alone bots, an IRC daemon bot cannot be run using the L<botnet> command. Instead, they must be installed by a specific server and then are run as part of server startup via the L<botnet> command.
 
 =head1 METHODS
 
-=head2 setup
-
-Adds the IRC component to the bot's memory on setup. 
-
-=cut
-
-sub setup {
-    my $self  = shift;
-    my $brain = shift;
-
-    $brain->remember(
-        [ 'irc' ] => POE::Component::IRC::State->spawn( alias => 'irc' )
-    );
-}
-
 =head2 default_configuration PACKAGE
 
-Returns a base configuration for an IRC bot.
+Returns a bas configuration for an IRC daemon bot.
 
 =cut
 
@@ -77,28 +95,24 @@ sub default_configuration {
     $default_channel =~ s/\W+/_/g;
 
     return {
-        irc_connect => {
-            nick     => $name,
-            username => lc $name,
-            ircname  => Bot::Net->short_name_for_bot($package),
+        alias        => $name,
 
-            server   => 'localhost',
-            port     => 6667,
-
-            flood    => 1,
+        spoofing     => {
+            nick    => $name,
+            ircname => $name,
         },
-        
-        channels => [ '#'.$default_channel ],
+
+        channels     => [ '#'.$default_channel ],
     };
 }
 
 =head1 BOT STATES
 
-The following states are avaiable for your bot to implement.
+The following states are available for your bot to implement.
 
 =head2 on bot connected
 
-This state is emitted as soon as the bot has connected to the server and the server has passed back a message. This means it is now safe to communicate with the server.
+This is emitted as soon as the nick spoofing and initial channel setup has been setup.
 
 =head2 on bot message_to_group EVENT
 
@@ -110,145 +124,109 @@ The C<EVENT> is a L<Bot::Net::Message> object setup to contain the group message
 
 This state occurs when either the bot observes a message sent to a channel that it is in that has been prefixed with "MyNick:" or a private message was sent directly to the bot.
 
-The C<EVENT> is a L<Bot::Net::Message> object setup for either a group or private message depending on which occurred. If a pblic message the "MyNick:" part of the message will have been stripped.
+The C<EVENT> is a L<Bot::Net::Message> object setup for either a group or private message depending on which occurred. If a public message the "MyNick:" part of the message will have been stripped.
 
 =head1 MIXIN STATES
 
 =head2 on _start
 
-Sets up the IRC client.
+Tells the server to spoof the bot's nick and registers to receive daemon events. It also sets up an alias for the session so that the server and other server bots may talk to your bot directly. The alias is set according to the "alias" configuration parameter.
+
+It ends by firing the L</on bot connected> state.
 
 =cut
 
 on _start => run {
-    post irc => register => 'all';
-    post irc => connect  => recall [ config => 'irc_connect' ];
-};
-
-=head2 on irc_001
-
-Connects to the channels that the bot has been configured to join. This then fires the L</on bot connected> event which indicates that it is now safe to issue IRC commands, if your bot needs to do so.
-
-=cut
-
-on irc_001 => run {
-    my $self = get OBJECT;
-    my $log  = recall 'log';
-
+    my $alias    = recall [ config => 'alias' ];
+    my $spoofed  = recall [ config => 'spoofing' ];
     my $channels = recall [ config => 'channels' ];
-    for my $channel (@$channels) {
-        $log->info("Joining $channel...");
-        post irc => join => $channel;
+
+    get(KERNEL)->alias_set($alias) if $alias;
+
+    recall('log')->info('Setting up nick spoofing as '
+        .$spoofed->{nick});
+    post ircd => register => 'all'; # TODO limit this to a subset
+    post ircd => add_spoofed_nick => $spoofed;
+
+    for my $channel (@{ $channels || [] }) {
+        recall('log')->info('Joining '.$channel);
+        post ircd => daemon_cmd_join => $spoofed->{nick}, $channel;
     }
 
     yield 'bot_connected';
 };
 
-=head2 on irc_disconnected
+=head2 on ircd_daemon_privmsg USERHOST, NICK, MESSAGE
 
-=head2 on irc_error
-
-=head2 on irc_socketerr
-
-Handles bot disconnections. If the bot becomes disconnected it will automatically attempt to reconnect until the server returns or lets the bot back. It will attempt to do so in a way that will not cause it to be blocked for flooding.
+Handles IRC messasges sent directly to the bot. This then emits the additional bot state L</on bot message_to_me>. It will be passed a single argument, the L<Bot::Net::Message> containting the message received.
 
 =cut
 
-on [ qw( irc_disconnected irc_error irc_socketerr ) ] => run {
-    # TODO XXX FIXME Add support for notifying the bot that the connection has
-    # been lost and then notifying the bot again when the connection is
-    # re-established.
-
-    delay attempt_reconnect => 60;
-};
-
-=head2 on attempt_reconnect
-
-This state is invoked by L</on irc_disconnected> and related states. This tells the IRC client to attempt ot reconnect. This will be called repeatedly (on a delay) until a connection is reestablished.
-
-=cut
-
-on attempt_reconnect => run {
-    post irc => 'connect';
-};
-
-=head2 irc_msg USERHOST, ME, MESSAGE
-
-Handles IRC messages sent directly to the bot. This emits:
-
-=over
-
-=item bot message_to_me
-
-See L</bot message_to_me>.
-
-=back
-
-=cut
-
-on irc_msg => run {
+on ircd_daemon_privmsg => run {
     my $userhost = get ARG0;
     my $me       = get ARG1;
     my $message  = get ARG2;
 
+    my $my_nick = recall [ config => spoofing => 'nick' ];
+
     my ($nick, $host) = split /!/, $userhost;
 
-    my $event = Bot::Net::Message->new({
-        sender_nick     => $nick,
-        sender_host     => $host,
-        recipient_nicks => $me,
-        message         => $message,
-        private         => 1,
-    });
+    # Only respond to messages directly to me
+    if ($me eq $my_nick) {
+        my $event = Bot::Net::Message->new({
+            sender_nick     => $nick,
+            sender_host     => $host,
+            recipient_nicks => $me,
+            message         => $message,
+            private         => 1,
+        });
 
-    yield bot_message_to_me => $event;
+        yield bot_message_to_me => $event;
+    }
 };
 
-=head2 irc_public USERHOST, CHANNEL, MESSAGE
+=head2 ircd_daemon_public USERHOST, CHANNEL, MESSAGE
 
-Handles IRC messages sent to a public channel. This then emits additional bot states:
+Handles any public messages stated in a channel the bot is in. It will emit either a L</on bot message_to_group> state or L</on bot message_to_me> state depending on whether or not the message was prefixed with "MyNick:".
 
-=over
-
-=item bot message_to_group
-
-Reports that a message was sent to a channel the bot is in. See L</bot message_to_group>.
-
-=item bot message_to_me
-
-Reports that a messages was sent to me publically in a channel. See L</bot message_to_me>.
-
-=back
+Both events will receive a single argument, the L<Bot::Net::Message> representing the message sent. The message will have the "MyNick:" prefix stripped on teh L</on bot message_to_me> message.
 
 =cut
 
-on irc_public => run {
+on ircd_daemon_public => run {
     my $userhost = get ARG0;
     my $channel  = get ARG1;
     my $message  = get ARG2;
 
-    my $my_nick = recall [ config => irc_connect => 'nick' ];
-
-    my $state;
-    if ($message =~ s/^\Q$my_nick\E:\s*//) {
-        $state = 'message_to_me';
-    }
-
-    else {
-        $state = 'message_to_group';
-    }
+    my $my_nick  = recall [ config => spoofing => 'nick' ];
+    my $channels = recall [ config => 'channels' ];
 
     my ($nick, $host) = split /!/, $userhost;
 
-    my $event = Bot::Net::Message->new({
-        sender_nick      => $nick,
-        sender_host      => $host,
-        recipient_groups => $channel,
-        message          => $message,
-        public           => 1,
-    });
+    # FIXME This isn't a very good mechanism for determining if this spoofed
+    # nick is in the channel since it could have changed.
 
-    yield 'bot_' . $state => $event;
+    # Am I in this channel?
+    if (grep { $_ eq $channel } @{ $channels || [] }) {
+        my $state;
+        if ($message =~ s/^\Q$my_nick\E:\s*//) {
+            $state = 'message_to_me';
+        }
+
+        else {
+            $state = 'message_to_group';
+        }
+
+        my $event = Bot::Net::Message->new({
+            sender_nick      => $nick,
+            sender_host      => $host,
+            recipient_groups => $channel,
+            message          => $message,
+            public           => 1,
+        });
+
+        yield 'bot_' . $state => $event;
+    }
 };
 
 =head2 send_to DESTINATION, MESSAGE
@@ -288,6 +266,7 @@ on send_to => run {
     my $full_message = get ARG1;
 
     my $log     = recall 'log';
+    my $my_nick = recall [ config => spoofing => 'nick' ];
 
     # Split the message up by newlines
     my @messages = split /\n/, $full_message;
@@ -384,7 +363,7 @@ on send_to => run {
             }
 
             # Post the message by chunks
-            post irc => privmsg => $channel => $_ 
+            post ircd => daemon_cmd_privmsg => $my_nick => $channel => $_ 
                 for $message_chunks->($message);
         }
     }
@@ -394,7 +373,7 @@ on send_to => run {
 
         # For each message line, post it to the nick in chunks
         for my $message (@messages) {
-            post irc => privmsg => $nick => $_
+            post ircd => daemon_cmd_privmsg => $my_nick => $nick => $_
                 for $message_chunks->($message);
         }
     }
@@ -477,11 +456,15 @@ This causes the IRC client to close down the connection and quit.
 =cut
 
 on bot_quit => run {
-    recall('log')->warn("Quitting the IRC connection.");
-    post irc => quit => 'Quitting.';
+    recall('log')->warn("Stopping spoofed server-side IRC bot.");
 
-    post irc => unregister => 'all';
-    forget 'irc';
+    my $alias   = recall [ config => 'alias' ];
+    my $my_nick = recall [ config => spoofing => 'nick' ];
+
+    post ircd => del_spoofed_nick => $my_nick => 'Quitting.';
+    post ircd => unregister => 'all';
+
+    get(KERNEL)->alias_remove($alias) if $alias;
 };
 
 =head1 AUTHORS
